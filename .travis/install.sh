dfba77c5c368d34b646d82b44744af9bd417b01c
shai atias <shai@experitest.com>
2019-04-21 15:14:44 +0300

    fixing the build

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,4 +21,10 @@ fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     echo "linux"
+
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    apt-get update
+    apt-get install ansible
+
+
 fi
