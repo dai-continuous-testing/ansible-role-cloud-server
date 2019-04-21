a7fcc7de34d610fc63423f15d4bf2ca60f6841a9
shai atias <shai@experitest.com>
2019-04-21 20:11:37 +0300

    doing some cleanup

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,23 +3,23 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo "osx"
 
-    brew cask install docker
-    open --background -a Docker
     
     sudo pip install ansible
 
-    DOCKER_BIN=/Applications/Docker.app/Contents/Resources/bin/docker
+    # brew cask install docker
+    # open --background -a Docker
+    # DOCKER_BIN=/Applications/Docker.app/Contents/Resources/bin/docker
 
-    while (! $DOCKER_BIN system info > /dev/null 2>&1 ); do
-        echo "Waiting for Docker to launch..."
-        sleep 1
-    done
+    # while (! $DOCKER_BIN system info > /dev/null 2>&1 ); do
+    #     echo "Waiting for Docker to launch..."
+    #     sleep 1
+    # done
 
-    $DOCKER_BIN ps -a
-    $DOCKER_BIN --version
-    $DOCKER_BIN run hello-world
+    # $DOCKER_BIN ps -a
+    # $DOCKER_BIN --version
+    # $DOCKER_BIN run hello-world
 
-    $DOCKER_BIN run -d -p 5432:5432 -e 'POSTGRES_PASSWORD=secretpassword' -e 'POSTGRES_DB=cloudserver' postgres:10
+    # $DOCKER_BIN run -d -p 5432:5432 -e 'POSTGRES_PASSWORD=secretpassword' -e 'POSTGRES_DB=cloudserver' postgres:10
 
 fi
 
