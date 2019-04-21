ad91518537f2fd1b248ecc4b39413bb894f5fe4e
shai atias <shai@experitest.com>
2019-04-21 12:36:19 +0300

    fixing the test

--- tests/01_sanity/start.sh
+++ tests/01_sanity/start.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 set -ev
 
+# IMAGE_NAME=centos:7
+
+# docker run \
+#     --rm=true \
+#     -it \
+#     -v `pwd`:/ansible-project:rw $IMAGE_NAME \
+#     /bin/bash 
+#     #-c "cd /ansible-project/tests/$TEST_NAME && ./start.sh"
+
+# TEST_NAME=01_sanity
+# cd /ansible-project/tests/$TEST_NAME
+
+# GIT_URL=https://github.com/ExperitestOfficial/ansible-role-cloud-server.git
+# TRAVIS_BRANCH=debt/experiment_ci_cd
+
 echo "[STEP 1] installing dependencies"
 
 yum -y update
-
-yum -y install git
+yum -y install git sudo
 
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
 pip install ansible
 
-
-# GIT_URL=$(git remote get-url origin)
-
 GIT_URL=$(git config remote.origin.url)
 
 ansible-galaxy install -r requirements.yml --force
-ansible-galaxy install git+$GIT_URL,$ --force
+ansible-galaxy install git+$GIT_URL,$TRAVIS_BRANCH --force
 
 echo "[STEP 1] installing dependencies - completed successfully"
 
