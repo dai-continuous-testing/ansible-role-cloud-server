f4fd7f985a8a978dd5c5696aeb5c892f1a7c6539
shai atias <shai@experitest.com>
2019-04-21 15:22:03 +0300

    fix installing ansible

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,9 +22,9 @@ fi
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     echo "linux"
 
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-    apt-get update
-    apt-get install ansible
-
+    sudo sh -c 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list'
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    sudo apt-get update
+    sudo apt-get install ansible -y
 
 fi
